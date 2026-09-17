4da7c2b6bc0949b67614ea0a6e7a03c55b7cf2cd
Xylos <Xylos000@outlook.com>
2026-09-17 20:44:21 +1000

    Create loader.js

---
+++ loader.js
@@ -0,0 +1,7 @@
+console.clear();
+
+console.log(
+  "%cOUTDATED \n%cGO TO HUB-ZERO.SITE TO GET NEW LOADER",
+  "font-size: 80px; font-weight: bold; color: red; text-shadow: 2px 2px black;",
+  "font-size: 30px; font-weight: bold; color: white; text-shadow: 1px 1px black;"
+);
